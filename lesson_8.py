c1c0c401e5749bc96e389804d6b2e87dfbb0d977
rodrickaheebwa <rodrickaheebwa@gmail.com>
2022-08-30 11:41:15 +0300

    updates

---
+++ lesson_8.py
@@ -0,0 +1,8 @@
+# sets
+
+# all elements of a set must be immutable (so no lists as elements)
+# a set can't be indexed because it's unordered
+
+# you can eliminate duplicates from a list by converting it into a set
+
+# functions

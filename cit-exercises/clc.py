6dcc5c62eba0daec9924716a005f88db97915140
rodrickaheebwa <rodrickaheebwa@gmail.com>
2022-09-05 19:11:51 +0300

    new changes

---
+++ cit-exercises/clc.py
@@ -0,0 +1,47 @@
+import sys
+
+def add(num1, num2):
+    print(num1 + num2)
+
+def subtract(num1, num2):
+    print(num1 - num2)
+
+def multiply(num1, num2):
+    print(num1 * num2)
+
+def divide(num1, num2):
+    print(num1/num2)
+
+def usage():
+    print("-----------------------------")
+    print("Simple command line calculator")
+    print("Performs operations of addition, subtraction, multiplication and division on TWO numbers")
+    print("Usage: clc.py <add/subtract/multiply/divide> <number1> <number2>")
+    print("-----------------------------")
+
+def calculator():
+    if len(sys.argv) == 4:
+        operator = sys.argv[1]
+        num1 = int(sys.argv[2])
+        num2 = int(sys.argv[3])
+        if operator == 'add':
+            add(num1, num2)
+        elif operator == 'subtract':
+            subtract(num1, num2)
+        elif operator == 'multiply':
+            multiply(num1, num2)
+        elif operator == 'divide':
+            divide(num1, num2)
+        else:
+            print("Invalid operation")
+
+    else:
+        usage()
+        sys.exit(-1)
+
+def main():
+    calculator()
+
+
+if __name__ == '__main__':
+    main()
--- lessons/lesson_9.py
+++ lessons/lesson_9.py
@@ -1,3 +1,10 @@
 # modules
 
 # it is important to use "if __name__ == '__main__'" in your programs
+
+# create a python virtual environment before downloading modules; in case projects differ in the versions of a module they will use.
+# seperate the dependencies of one project from the dependencies of another
+# the python version and module versions can't be changed there after
+# python -m venv venv
+# .\venv\Scripts\activate
+# the virtual environment is created in that project folder
